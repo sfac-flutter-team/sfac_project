4d3a8b70c13fa0b899472f628ccd2536566ebdd2
Shin <tlstmdgh8620@gmail.com>
2023-03-30 11:54:51 +0900

    로그인 부분

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,3 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:sfac_project/service/auth_service.dart';
 
-class AuthController extends GetxController {}
+class AuthController extends GetxController {
+  final Rxn<User> _user = Rxn();
+
+  User? get user => _user.value;
+
+  login(id, pw) => AuthService().login(id, pw);
+
+  logout() => AuthService().logout();
+
+  //Firebase에 유저값이 있으면 메인페이지로 이동, 아니면 로그인페이지
+  @override
+  void onInit() {
+    super.onInit();
+    FirebaseAuth.instance.authStateChanges().listen((value) {
+      _user(value);
+      if (value != null) {
+        //Get.offAllNamed은 이전 페이지 모두 삭제 후 이동
+        //Get.offAllNamed(내 팀 페이지로이동);
+      } else {
+        //Get.offAllNamed(로그인페이지?);
+      }
+    });
+  }
+}
--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,3 +1,31 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:sfac_project/controller/auth_controller.dart';
 
-class LoginController extends GetxController {}
+class LoginController extends GetxController {
+  var emailController = TextEditingController();
+  var pwController = TextEditingController();
+
+  RxBool isButtonActivate = false.obs; //로그인 버튼 비활성화
+
+  //로그인 불러오고 값이 없으면 스낵바출력
+  login() async {
+    if (await Get.find<AuthController>()
+        .login(emailController.text, pwController.text)) {
+      // Get.offAllNamed(메인페이지로 이동);
+    } else {
+      Get.snackbar('로그인 오류!!', '존재하지 않는 회원 정보입니다.',
+          duration: const Duration(seconds: 3),
+          snackPosition: SnackPosition.BOTTOM);
+    }
+  }
+
+  //로그인버튼 활성화하는 함수
+  activateButton() {
+    if (emailController.text.isNotEmpty && pwController.text.isNotEmpty) {
+      isButtonActivate(true);
+    } else {
+      isButtonActivate;
+    }
+  }
+}
