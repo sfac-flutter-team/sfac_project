a2c8b2d101de477242db1dc107f22b8ffbe2ad32
Kim-kwan-woo <kw2577@naver.com>
2023-04-10 00:49:34 +0900

    로그인 페이지 ui 적용

---
+++ lib/view/widget/app_text_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:sfac_project/util/app_color.dart';
+import 'package:sfac_project/util/app_text_style.dart';
+
+class AppTextButton extends StatelessWidget {
+  const AppTextButton(
+      {super.key, required this.childText, required this.onPressed});
+
+  final String childText;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        foregroundColor: AppColor.subGrey,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        style: AppTextStyle.bKorPreRegular14(color: AppColor.subLightGrey),
+        childText,
+      ),
+    );
+  }
+}
