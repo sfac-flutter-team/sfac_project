379f5f659f9481424616d29c97b3017f7eb08391
Kim-kwan-woo <kw2577@naver.com>
2023-04-09 19:43:55 +0900

    라인업 페이지 구현 완료

--- lib/model/player.dart
+++ lib/model/player.dart
@@ -1,23 +1,37 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Player {
   String name; //선수 이름
-  String photo; //선수 사진
-  int assists; //어시스트 횟수
-  int total; //총 득점 횟수
+  String? photo; //선수 사진
+  int? number; //선수 번호
+  String? pos; //포지션
+  int? assists; //어시스트 횟수
+  int? total; //총 득점 횟수
 
   Player({
     required this.name,
-    required this.photo,
-    required this.assists,
-    required this.total,
+    this.photo,
+    this.number,
+    this.pos,
+    this.assists,
+    this.total,
   });
 
   factory Player.fromMap(Map<String, dynamic> map) {
     return Player(
       name: map['player']['name'],
-      photo: map['player']['photo'],
-      assists: map['statistics'].first['goals']['assists'],
-      total: map['statistics'].first['goals']['total'],
+      photo: map['player']['photo'] != null
+          ? map['player']['photo'] as String
+          : null,
+      number: map['player']['number'] != null
+          ? map['player']['number'] as int
+          : null,
+      pos: map['player']['pos'] != null ? map['player']['pos'] as String : null,
+      assists: map['statistics'] != null
+          ? map['statistics'].first['goals']['assists'] as int
+          : null,
+      total: map['statistics'] != null
+          ? map['statistics'].first['goals']['total'] as int
+          : null,
     );
   }
 }
--- lib/view/page/lineup_page.dart
+++ lib/view/page/lineup_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:sfac_project/controller/lineup_controller.dart';
 
@@ -13,7 +12,99 @@ class LineupPage extends GetView<LineupController> {
       appBar: AppBar(
         title: const Text('라인업'),
       ),
-      body: Obx(() => Text(controller.tempStr.value)),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            const Text(
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              '홈 팀',
+            ),
+            const Text('선발 선수'),
+            Obx(
+              () => ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: controller.homeTeamLineup.value?.players.length ?? 0,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        controller.homeTeamLineup.value!.players[index].name),
+                    leading: CircleAvatar(
+                      child: Text(controller
+                          .homeTeamLineup.value!.players[index].number
+                          .toString()),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const Text('후보 선수'),
+            Obx(
+              () => ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount:
+                    controller.homeTeamLineup.value?.substitutes.length ?? 0,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(controller
+                        .homeTeamLineup.value!.substitutes[index].name),
+                    leading: CircleAvatar(
+                      child: Text(controller
+                          .homeTeamLineup.value!.substitutes[index].number
+                          .toString()),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const Text(
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              '어웨이 팀',
+            ),
+            const Text('선발 선수'),
+            Obx(
+              () => ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: controller.awayTeamLineup.value?.players.length ?? 0,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        controller.awayTeamLineup.value!.players[index].name),
+                    leading: CircleAvatar(
+                      child: Text(controller
+                          .awayTeamLineup.value!.players[index].number
+                          .toString()),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const Text('후보 선수'),
+            Obx(
+              () => ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount:
+                    controller.awayTeamLineup.value?.substitutes.length ?? 0,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(controller
+                        .awayTeamLineup.value!.substitutes[index].name),
+                    leading: CircleAvatar(
+                      child: Text(controller
+                          .awayTeamLineup.value!.substitutes[index].number
+                          .toString()),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
