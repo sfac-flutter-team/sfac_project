959c4c5762800470c223560791b5b6f7747eee2d
rlaalswls63 <rlaalswls63@gmail.com>
2023-04-12 16:11:44 +0900

    ranking controller

--- lib/controller/ranking_controller.dart
+++ lib/controller/ranking_controller.dart
@@ -6,40 +6,23 @@ import 'package:sfac_project/model/standing.dart';
 import 'package:sfac_project/service/db_service.dart';
 
 class RankingController extends GetxController {
-RxList<QueryDocumentSnapshot<Standing>> standingInfo = RxList<QueryDocumentSnapshot<Standing>>();
-Rxn<QueryDocumentSnapshot<dynamic>> playerInfo = Rxn<QueryDocumentSnapshot<dynamic>>();
-final standingRef = FirebaseFirestore.instance
-      .collection('standings')
-      .withConverter(
-          fromFirestore: (snapshot, _) => Standing.fromMap(snapshot.data()!),
-          toFirestore: (standing, _) => {});
-
-final playerRef = FirebaseFirestore.instance
-      .collection('players')
-      .withConverter(
-          fromFirestore: (snapshot, _) => snapshot
-              .data()!['topscorers']
-              .map((value) => Player.fromMap(value))
-              .toList(),
-          toFirestore: (player, _) => {});
-
-  Future readData()async{
+  RxList<QueryDocumentSnapshot<Standing>> standingInfo = RxList<QueryDocumentSnapshot<Standing>>();
+  Rxn<QueryDocumentSnapshot<dynamic>> playerInfo = Rxn<QueryDocumentSnapshot<dynamic>>();
+  Future readData() async {
     standingInfo.value = await DBService().readStandings();
     playerInfo.value = await DBService().readTopScorers();
   }
 
-@override
-  void onInit(){
+  @override
+  void onInit() {
     super.onInit();
     readData();
   }
-PageController pageController = PageController();
+
+  PageController pageController = PageController();
   RxInt selectedIndex = 0.obs;
-handleRankingOnPressed(int index) {
-   selectedIndex(index);
-  pageController.jumpToPage(selectedIndex.value);
+  handleRankingOnPressed(int index) {
+    selectedIndex(index);
+    pageController.jumpToPage(selectedIndex.value);
   }
-
-
-
 }
