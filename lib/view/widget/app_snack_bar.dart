7166138543944de23423232e4ede5809337598de
Kim-kwan-woo <kw2577@naver.com>
2023-04-19 11:00:35 +0900

    ui 수정

--- lib/view/widget/app_snack_bar.dart
+++ lib/view/widget/app_snack_bar.dart
@@ -4,13 +4,12 @@ class AppSnackBar {
   static void showSnackBar(BuildContext context, String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
+        backgroundColor: Colors.black,
         content: Center(child: Text(message)),
-        duration: Duration(seconds: 2),
+        duration: const Duration(seconds: 2),
         behavior: SnackBarBehavior.floating,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        margin: EdgeInsets.symmetric(
-          vertical: 10,
-        ),
+        margin: const EdgeInsets.all(20),
       ),
     );
   }
